90578559fca67e2937afb553a27b143c64985bb4
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-09-22 12:55:58 +0100

    2-execute_a_command.pp

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Define a Puppet manifest to kill a process named "killmenow"
+exec { 'pkill -f killmenow':
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif      => 'pgrep -f killmenow',
+  refreshonly => true,
+}
