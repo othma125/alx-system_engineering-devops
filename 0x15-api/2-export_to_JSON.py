42d147ca52f416575eb5a924db1939751877727a
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-23 15:18:44 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""fetches information from JSONplaceholder API and exports to JSON"""
+
+from json import dump
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + f"/user/{sys.argv[1]}/todos"
+    name_url = main_url + f"/users/{sys.argv[1]}"
+    todo_result = requests.get(todo_url).json()
+    name_result = requests.get(name_url).json()
+    todo_list = []
+    for todo in todo_result:
+        my_dict = {"user_ID": sys.argv[1],
+                   "username": name_result.get("username"),
+                   "completed": todo.get('completed'),
+                   "task": todo.get("title")}
+        todo_list.append(my_dict)
+    with open(f"{sys.argv[1]}.json", 'w') as f:
+        dump({sys.argv[1]: todo_list}, f)
