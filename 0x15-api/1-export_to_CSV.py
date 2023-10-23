a50ab86fd69a0d68daa2fc00d697807ee5142154
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-23 15:11:10 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,24 +2,24 @@
 """fetches information from JSONplaceholder API and exports to CSV"""
 
 from csv import DictWriter, QUOTE_ALL
-from requests import get
-from sys import argv
+import requests
+import sys
 
 if __name__ == "__main__":
     main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + f"/user/{argv[1]}/todos"
-    name_url = main_url + f"/users/{argv[1]}"
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    todo_url = main_url + f"/user/{sys.argv[1]}/todos"
+    name_url = main_url + f"/users/{sys.argv[1]}"
+    todo_result = requests.get(todo_url).json()
+    name_result = requests.get(name_url).json()
     todo_list = []
     for todo in todo_result:
         if todo.get("completed"):
-            my_dict = {"user_ID": argv[1],
+            my_dict = {"user_ID": sys.argv[1],
                        "username": name_result.get("username"),
                        "completed": todo.get('completed'),
                        "task": todo.get("title")}
             todo_list.append(my_dict)
-    with open("{}.csv".format(argv[1]), 'w', newline='') as f:
+    with open(f"{sys.argv[1]}.csv", 'w', newline='') as f:
         header = ["user_ID", "username", "completed", "task"]
         writer = DictWriter(f, fieldnames=header, quoting=QUOTE_ALL)
         writer.writerows(todo_list)
