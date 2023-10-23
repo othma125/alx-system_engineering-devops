e94f1fff9563cb69622af4644fd4b1858abe3a75
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-10-23 15:09:42 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,17 +3,16 @@
 Uses the JSON placeholder api to query data about an employee
 """
 
-from sys import argv
-from requests import get
-
 # from pprint import pprint
+import requests
+import sys
 
 if __name__ == '__main__':
     main_url = 'https://jsonplaceholder.typicode.com'
-    todo_url = main_url + f"/user/{argv[1]}/todos"
-    name_url = main_url + f"/users/{argv[1]}"
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    todo_url = main_url + f"/user/{sys.argv[1]}/todos"
+    name_url = main_url + f"/users/{sys.argv[1]}"
+    todo_result = requests.get(todo_url).json()
+    name_result = requests.get(name_url).json()
     # pprint(todo_result)
     # pprint(name_result)
     n = len([todo for todo in todo_result
