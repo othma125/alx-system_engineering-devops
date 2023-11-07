7008f596d97098c1542d079b46b406f9d9f776e3
Othmane EL YAAKOUBI <othma125@hotmail.com>
2023-11-07 17:12:37 +0100

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -11,17 +11,16 @@ def count_words(subreddit, word_list, after="", word_dict={}):
         for word in word_list:
             lower = word.lower()
             if lower not in new_word_list:
-                new_word_list.append(word.lower())
+                new_word_list.append(lower)
         word_list = new_word_list
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
-    headers = {"User-Agent": "My-User-Agent"}
-    response = get(url, headers=headers, allow_redirects=False)
+    response = get(url, headers={"User-Agent": "My-User-Agent"},
+                   allow_redirects=False)
     if response.status_code == 200:
         data = response.json().get("data")
         for child in data.get("children"):
-            text_list = [txt.lower() for txt
-                         in child.get("data").get("title").split()]
-            for word in text_list:
+            for word in [txt.lower() for txt
+                         in child.get("data").get("title").split()]:
                 if word in word_list:
                     if word in word_dict:
                         word_dict[word] += 1
